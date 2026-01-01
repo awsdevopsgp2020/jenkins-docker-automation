67c257eb6b3f862abd44bb3a4a7f823ed66b79b5
awsdevopsgp2020 <120734577+awsdevopsgp2020@users.noreply.github.com>
2026-01-01 17:27:25 +0530

    Change ingress port from 8080 to 8081

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
