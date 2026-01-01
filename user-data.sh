99a5884f0ed15d2c8e730a68f3b3892fe56b6454
awsdevopsgp2020 <120734577+awsdevopsgp2020@users.noreply.github.com>
2026-01-01 17:29:08 +0530

    Refactor user-data.sh for better command clarity
    Updated user-data.sh to improve command usage and added Jenkins port change.

--- user-data.sh
+++ user-data.sh
@@ -1,12 +1,14 @@
-
 #!/bin/bash
+set -e
+
+# Update system
 apt update -y
 
 # Install Java
-apt install openjdk-17-jdk -y
+apt install -y openjdk-17-jdk
 
 # Install Docker
-apt install docker.io -y
+apt install -y docker.io git
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ubuntu
@@ -20,7 +22,25 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 apt update -y
-apt install jenkins -y
+apt install -y jenkins
 
+# 🔥 CHANGE JENKINS PORT TO 8081
+sed -i 's/HTTP_PORT=8080/HTTP_PORT=8081/' /etc/default/jenkins
+
+# Permissions
 usermod -aG docker jenkins
+
+# Restart services
+systemctl daemon-reexec
 systemctl restart jenkins
+systemctl restart docker
+
+# -----------------------------
+# DEPLOY SAMPLE WEB APP
+# -----------------------------
+cd /home/ubuntu
+git clone https://github.com/awsdevopsgp2020/git-jenkins-docker.git
+cd git-jenkins-docker/webapp
+
+docker build -t webapp .
+docker run -d -p 80:80 --name webapp webapp
